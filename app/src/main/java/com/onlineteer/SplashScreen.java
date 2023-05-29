6c65084e55fe26de1f49a7b5e61cdae1c6499c8f
SakuraGlory <96517196+SakuraGlory@users.noreply.github.com>
2023-05-29 18:59:15 +0530

    2

--- app/src/main/java/com/onlineteer/SplashScreen.java
+++ app/src/main/java/com/onlineteer/SplashScreen.java
@@ -1,13 +1,20 @@
 package com.onlineteer;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.onlineteer.auth.Login_screen;
+
 public class SplashScreen extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        startActivity(new Intent(this, Login_screen.class));
+
     }
 }
--- app/src/main/java/com/onlineteer/auth/Login_screen.java
+++ app/src/main/java/com/onlineteer/auth/Login_screen.java
@@ -1,16 +1,25 @@
 package com.onlineteer.auth;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.onlineteer.Home_Screen;
 import com.onlineteer.R;
 
 public class Login_screen extends AppCompatActivity {
+
+    AppCompatButton login_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+        login_btn = findViewById(R.id.login_btn);
+        login_btn.setOnClickListener(view -> {
+            startActivity(new Intent(this,Home_Screen.class));
+        });
     }
 }
